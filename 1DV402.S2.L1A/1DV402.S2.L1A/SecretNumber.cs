3d1b6fadcc16d2635b99da1c3969fab8a0713e9e
ss223ek <ss223ek@student.lnu.se>
2014-10-05 18:58:23 +0200

    Inga kompileringsfel, dags att testa med test.cs Signed-off-by: ss223ek <ss223ek@student.lnu.se>

--- 1DV402.S2.L1A/1DV402.S2.L1A/SecretNumber.cs
+++ 1DV402.S2.L1A/1DV402.S2.L1A/SecretNumber.cs
@@ -10,24 +10,60 @@ namespace _1DV402.S2.L1A
     {
         private int _count;
         private int _number;
-        private const int MaxNumberOfGuesses;
+        public const int MaxNumberOfGuesses = 7;
 
 
-        //här ska jag lägga åtkomstmodiferare Get och Set med villkor
 
-        public void Initializa()
+
+        public void Initialize()
         {
-        
+            Random randomTool = new Random();       //Skapar ett slumpobjekt
+            _number = randomTool.Next(0, 100);       //Anropar metod som slumpat from 0 tom 100
+            _count = 0;                             //nollställer räknare för fältet
         }
 
         public bool MakeGuess(int number)
         {
+            _count++;
+            if(_count>7)                            //fler än 7 gissningar inte tillåtet (sid 7)
+            {
+                throw new ApplicationException();
+            }
+
+            if(0>number||100<number)
+            {
+            throw new ArgumentOutOfRangeException();    //anropad med fel värden
+            }
+
+            // för högt, för lågt eller rätt
+            if(number>_number)
+            {
+                Console.WriteLine("{0} är för högt. Du har {1} gissningar kvar.)",number,(MaxNumberOfGuesses-_count));
+            }
+
+            else if (number < _number)
+            {
+                Console.WriteLine("{0} är för lågt. Du har {1} gissningar kvar.)", number, (MaxNumberOfGuesses - _count));
+            }
+
+            else 
+            {
+                Console.WriteLine("RÄTT GISSAT! Du klarade det på {0} försök.)", _count);
+                Initialize();
+                return true;
+            }
+
+            // i detta läge vet vi att gissningen är fel, nu kollar vi om det finns gissningar kvar
+            if(7<=_count)
+            {
+                Console.WriteLine("Det hemliga talet är {0}.",_number);
+            }
         return false;
         }
 
         public SecretNumber()
         {
-        
+            Initialize();           //borde väl räcka? jag vet ju att värden är rätt då.
         }
     }
 }
