21e78d436548d520978e3c128f14151aa6b41ab2
ss223ek <ss223ek@student.lnu.se>
2014-10-05 13:56:23 +0200

    Grundstruktur med rätt namn på metoder Signed-off-by: ss223ek <ss223ek@student.lnu.se>

---
+++ 1DV402.S2.L1A/1DV402.S2.L1A/SecretNumber.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1DV402.S2.L1A
+{
+    class SecretNumber
+    {
+        private int _count;
+        private int _number;
+        private const int MaxNumberOfGuesses;
+
+
+        //här ska jag lägga åtkomstmodiferare Get och Set med villkor
+
+        public void Initializa()
+        {
+        
+        }
+
+        public bool MakeGuess(int number)
+        {
+        return false;
+        }
+
+        public SecretNumber()
+        {
+        
+        }
+    }
+}
